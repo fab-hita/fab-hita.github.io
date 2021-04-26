d3ef1d04118e572ca98782b7facacce67c800d39
Fabrice <fabrice@hita.co.za>
2021-04-26 11:29:16 +0200

    Testing translate

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function moveRight(){
+    var card1 = document.getElementById('card1');
+    var card2 = document.getElementById('card2');
+    var card3 = document.getElementById('card3');
+    var card4 = document.getElementById('card4');
+    var card5 = document.getElementById('card5');
+    
+    card1.style.transform = "translate(1120px)";
+    card2.style.transform = "translate(-224px)";
+    card4.style.transform = "translate(-672px)";
+    card5.style.transform = "translate(-224px)";
+    
+}
